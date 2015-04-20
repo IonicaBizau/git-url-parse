70637be9f020f11c65490a84256f41cf7c49ef09
Ionică Bizău <bizauionica@yahoo.com>
2015-04-20 17:49:37 +0300

    Specs for git+ssh

--- test/index.js
+++ test/index.js
@@ -4,41 +4,55 @@ var GitUrlParse = require("../lib")
   ;
 
 // Constants
-const urls = {
+const URLS = {
     ssh: "git@github.com:IonicaBizau/node-giturlparse.git"
   , https: "https://github.com/IonicaBizau/node-giturlparse.git"
-  , local: "/opt/git/project.git"
-  , file: "file:///opt/git/project.git"
+  , gitSsh: "git+ssh://git@github.com/IonicaBizau/node-giturlparse.git"
 };
 
+// SSH urls
 it("should parse ssh urls", function (cb) {
-    var res = GitUrlParse(urls.ssh);
+    var res = GitUrlParse(URLS.ssh);
     Assert.strictEqual(res.protocol, "ssh");
     Assert.strictEqual(res.source, "github.com");
     Assert.strictEqual(res.owner, "IonicaBizau");
     Assert.strictEqual(res.name, "node-giturlparse");
-    Assert.strictEqual(res._, urls.ssh);
-    Assert.strictEqual(res.toString("https"), urls.https);
+    Assert.strictEqual(res._, URLS.ssh);
+    Assert.strictEqual(res.toString("https"), URLS.https);
     cb();
 });
 
+// HTTPS urls
 it("should parse https urls", function (cb) {
-    var res = GitUrlParse(urls.https);
+    var res = GitUrlParse(URLS.https);
     Assert.strictEqual(res.protocol, "https");
     Assert.strictEqual(res.source, "github.com");
     Assert.strictEqual(res.owner, "IonicaBizau");
     Assert.strictEqual(res.name, "node-giturlparse");
-    Assert.strictEqual(res._, urls.https);
-    Assert.strictEqual(res.toString("ssh"), urls.ssh);
+    Assert.strictEqual(res._, URLS.https);
+    Assert.strictEqual(res.toString("ssh"), URLS.ssh);
     cb();
 });
 
+// HTTPS with ending slash
 it("should parse https urls with ending slash", function (cb) {
     var res = GitUrlParse("https://github.com/IonicaBizau/node-giturlparse/");
     Assert.strictEqual(res.protocol, "https");
     Assert.strictEqual(res.source, "github.com");
     Assert.strictEqual(res.owner, "IonicaBizau");
     Assert.strictEqual(res.name, "node-giturlparse");
-    Assert.strictEqual(res.toString("ssh"), urls.ssh);
+    Assert.strictEqual(res.toString("ssh"), URLS.ssh);
+    cb();
+});
+
+// HTTPS with ending slash
+it("should parse git+ssh urls", function (cb) {
+    var res = GitUrlParse(URLS.gitSsh);
+    Assert.strictEqual(res.protocol, "git+ssh");
+    Assert.strictEqual(res.source, "github.com");
+    Assert.strictEqual(res.owner, "IonicaBizau");
+    Assert.strictEqual(res.name, "node-giturlparse");
+    Assert.strictEqual(res.toString("ssh"), URLS.ssh);
+    Assert.strictEqual(res.toString("https"), URLS.https);
     cb();
 });
