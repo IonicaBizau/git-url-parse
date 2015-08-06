503b28cba122beb199000f2cab2836ce6fc07848
Ionică Bizău <bizauionica@gmail.com>
2015-08-06 18:55:44 +0300

    Added the tests from issues

--- test/index.js
+++ test/index.js
@@ -17,7 +17,7 @@ it("should parse ssh urls", function (cb) {
     Assert.strictEqual(res.source, "github.com");
     Assert.strictEqual(res.owner, "IonicaBizau");
     Assert.strictEqual(res.name, "node-giturlparse");
-    Assert.strictEqual(res._, URLS.ssh);
+    Assert.strictEqual(res.href, URLS.ssh);
     Assert.strictEqual(res.toString("https"), URLS.https);
     Assert.strictEqual(res.toString("git+ssh"), URLS.gitSsh);
     Assert.strictEqual(res.toString("ssh"), URLS.ssh);
@@ -31,7 +31,7 @@ it("should parse https urls", function (cb) {
     Assert.strictEqual(res.source, "github.com");
     Assert.strictEqual(res.owner, "IonicaBizau");
     Assert.strictEqual(res.name, "node-giturlparse");
-    Assert.strictEqual(res._, URLS.https);
+    Assert.strictEqual(res.href, URLS.https);
     Assert.strictEqual(res.toString("https"), URLS.https);
     Assert.strictEqual(res.toString("git+ssh"), URLS.gitSsh);
     Assert.strictEqual(res.toString("ssh"), URLS.ssh);
@@ -54,12 +54,48 @@ it("should parse https urls with ending slash", function (cb) {
 // git+ssh protocol
 it("should parse git+ssh urls", function (cb) {
     var res = GitUrlParse(URLS.gitSsh);
-    Assert.strictEqual(res.protocol, "git+ssh");
+    Assert.strictEqual(res.protocol, "ssh");
     Assert.strictEqual(res.source, "github.com");
     Assert.strictEqual(res.owner, "IonicaBizau");
     Assert.strictEqual(res.name, "node-giturlparse");
     Assert.strictEqual(res.toString("https"), URLS.https);
     Assert.strictEqual(res.toString("git+ssh"), URLS.gitSsh);
     Assert.strictEqual(res.toString("ssh"), URLS.ssh);
+    cb();
+});
+
+// oauth
+it("should parse oauth urls", function (cb) {
+    var res = GitUrlParse("https://token:x-oauth-basic@github.com/owner/name.git");
+    Assert.strictEqual(res.source, "github.com");
+    Assert.strictEqual(res.owner, "owner");
+    Assert.strictEqual(res.name, "name");
+    cb();
+});
+
+// oauth bitbucket
+it("should parse Bitbucket oauth urls", function (cb) {
+    var res = GitUrlParse("https://x-token-auth:token@bitbucket.org/owner/name.git");
+    Assert.strictEqual(res.source, "bitbucket.org");
+    Assert.strictEqual(res.owner, "owner");
+    Assert.strictEqual(res.name, "name");
+    cb();
+});
+
+// https bitbucket
+it("should parse Bitbucket https urls", function (cb) {
+    var res = GitUrlParse("https://owner@bitbucket.org/owner/name");
+    Assert.strictEqual(res.source, "bitbucket.org");
+    Assert.strictEqual(res.owner, "owner");
+    Assert.strictEqual(res.name, "name");
+    cb();
+});
+
+// https cloudforge
+it("should parse CloudForge urls", function (cb) {
+    var res = GitUrlParse("https://owner@organization.git.cloudforge.com/name.git");
+    Assert.strictEqual(res.source, "cloudforge.com");
+    Assert.strictEqual(res.owner, "owner");
+    Assert.strictEqual(res.name, "name");
     cb();
 });
