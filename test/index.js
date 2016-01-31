bd13d8ba40a8f7865ed38dcef9e92c27f2e7fa58
Ionică Bizău <bizauionica@gmail.com>
2016-01-31 11:15:04 +0200

    Removed the .git suffix for http(s) urls

--- test/index.js
+++ test/index.js
@@ -5,20 +5,21 @@ const gitUrlParse = require("..")
 
 // Constants
 const URLS = {
-    ssh: "git@github.com:IonicaBizau/node-giturlparse.git"
-  , https: "https://github.com/IonicaBizau/node-giturlparse.git"
-  , gitSsh: "git+ssh://git@github.com/IonicaBizau/node-giturlparse.git"
+    ssh: "git@github.com:IonicaBizau/git-url-parse.git"
+  , https: "https://github.com/IonicaBizau/git-url-parse"
+  , gitSsh: "git+ssh://git@github.com/IonicaBizau/git-url-parse.git"
 };
 
 tester.describe("parse urls", test => {
 
     // SSH urls
-    test.it("should parse ssh urls", () => {
+    test.should("parse ssh urls", () => {
         var res = gitUrlParse(URLS.ssh);
         test.expect(res.protocol).toBe("ssh");
         test.expect(res.source).toBe("github.com");
         test.expect(res.owner).toBe("IonicaBizau");
-        test.expect(res.name).toBe("node-giturlparse");
+        test.expect(res.name).toBe("git-url-parse");
+        test.expect(res.full_name).toBe("IonicaBizau/git-url-parse");
         test.expect(res.href).toBe(URLS.ssh);
         test.expect(res.toString("https")).toBe(URLS.https);
         test.expect(res.toString("git+ssh")).toBe(URLS.gitSsh);
@@ -26,12 +27,12 @@ tester.describe("parse urls", test => {
     });
 
     // HTTPS urls
-    test.it("should parse https urls", () => {
+    test.should("parse https urls", () => {
         var res = gitUrlParse(URLS.https);
         test.expect(res.protocol).toBe("https");
         test.expect(res.source).toBe("github.com");
         test.expect(res.owner).toBe("IonicaBizau");
-        test.expect(res.name).toBe("node-giturlparse");
+        test.expect(res.name).toBe("git-url-parse");
         test.expect(res.href).toBe(URLS.https);
         test.expect(res.toString("https")).toBe(URLS.https);
         test.expect(res.toString("git+ssh")).toBe(URLS.gitSsh);
@@ -39,31 +40,31 @@ tester.describe("parse urls", test => {
     });
 
     // HTTPS with ending slash
-    test.it("should parse https urls with ending slash", () => {
-        var res = gitUrlParse("https://github.com/IonicaBizau/node-giturlparse/");
+    test.should("parse https urls with ending slash", () => {
+        var res = gitUrlParse("https://github.com/IonicaBizau/git-url-parse/");
         test.expect(res.protocol).toBe("https");
         test.expect(res.source).toBe("github.com");
         test.expect(res.owner).toBe("IonicaBizau");
-        test.expect(res.name).toBe("node-giturlparse");
+        test.expect(res.name).toBe("git-url-parse");
         test.expect(res.toString("https")).toBe(URLS.https);
         test.expect(res.toString("git+ssh")).toBe(URLS.gitSsh);
         test.expect(res.toString("ssh")).toBe(URLS.ssh);
     });
 
     // git+ssh protocol
-    test.it("should parse git+ssh urls", () => {
+    test.should("parse git+ssh urls", () => {
         var res = gitUrlParse(URLS.gitSsh);
         test.expect(res.protocol).toBe("ssh");
         test.expect(res.source).toBe("github.com");
         test.expect(res.owner).toBe("IonicaBizau");
-        test.expect(res.name).toBe("node-giturlparse");
+        test.expect(res.name).toBe("git-url-parse");
         test.expect(res.toString("https")).toBe(URLS.https);
         test.expect(res.toString("git+ssh")).toBe(URLS.gitSsh);
         test.expect(res.toString("ssh")).toBe(URLS.ssh);
     });
 
     // oauth
-    test.it("should parse oauth urls", () => {
+    test.should("parse oauth urls", () => {
         var res = gitUrlParse("https://token:x-oauth-basic@github.com/owner/name.git");
         test.expect(res.source).toBe("github.com");
         test.expect(res.owner).toBe("owner");
@@ -71,7 +72,7 @@ tester.describe("parse urls", test => {
     });
 
     // oauth bitbucket
-    test.it("should parse Bitbucket oauth urls", () => {
+    test.should("parse Bitbucket oauth urls", () => {
         var res = gitUrlParse("https://x-token-auth:token@bitbucket.org/owner/name.git");
         test.expect(res.source).toBe("bitbucket.org");
         test.expect(res.owner).toBe("owner");
@@ -79,7 +80,7 @@ tester.describe("parse urls", test => {
     });
 
     // https bitbucket
-    test.it("should parse Bitbucket https urls", () => {
+    test.should("parse Bitbucket https urls", () => {
         var res = gitUrlParse("https://owner@bitbucket.org/owner/name");
         test.expect(res.source).toBe("bitbucket.org");
         test.expect(res.owner).toBe("owner");
@@ -87,11 +88,27 @@ tester.describe("parse urls", test => {
     });
 
     // https cloudforge
-    test.it("should parse CloudForge urls", () => {
+    test.should("parse CloudForge urls", () => {
         var res = gitUrlParse("https://owner@organization.git.cloudforge.com/name.git");
         test.expect(res.source).toBe("cloudforge.com");
         test.expect(res.owner).toBe("owner");
         test.expect(res.organization).toBe("organization");
         test.expect(res.name).toBe("name");
+    });
+
+    test.should("parse subdomains", () => {
+        var res = gitUrlParse("https://gist.github.com/owner/id");
+        test.expect(res.source).toBe("github.com");
+        test.expect(res.resource).toBe("gist.github.com");
+        test.expect(res.owner).toBe("owner");
+        test.expect(res.name).toBe("id");
+        test.expect(res.toString()).toBe("https://gist.github.com/owner/id");
+
+        res = gitUrlParse("https://gist.github.com/id");
+        test.expect(res.source).toBe("github.com");
+        test.expect(res.resource).toBe("gist.github.com");
+        test.expect(res.owner).toBe("");
+        test.expect(res.name).toBe("id");
+        test.expect(res.toString()).toBe("https://gist.github.com/id");
     });
 });
