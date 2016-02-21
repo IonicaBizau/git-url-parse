d9731135e5cfb5a9e0729161bfe430c5d0d87f89
Ionică Bizău <bizauionica@gmail.com>
2016-02-21 10:39:19 +0200

    Rebuild

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,323 @@
-"use strict";var _m=module;!function r(e,t,n){function o(i,u){if(!t[i]){if(!e[i]){var l="function"==typeof require&&require;if(!u&&l)return l(i,!0);if(s)return s(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var c=t[i]={exports:{}};e[i][0].call(c.exports,function(r){var t=e[i][1][r];return o(t?t:r)},c,c.exports,r,e,t,n)}return t[i].exports}for(var s="function"==typeof require&&require,i=0;i<n.length;i++)o(n[i]);return o}({1:[function(r,e,t){function n(r){if("string"!=typeof r)throw new Error("The url must be a string.");var e=o(r),t=e.resource.split("."),s=null;switch(e.toString=function(r){return n.stringify(this,r)},e.source=t.length>2?t.slice(-2).join("."):e.source=e.resource,e.name=e.pathname.substring(1).replace(/\.git$/,""),e.owner=e.user,e.organization=e.owner,e.source){case"cloudforge.com":e.owner=e.user,e.organization=t[0];break;default:s=e.name.split("/"),2===s.length&&(e.owner=s[0],e.name=s[1])}return e.full_name=e.owner,e.name&&(e.full_name&&(e.full_name+="/"),e.full_name+=e.name),e}e=_m;var o=r("git-up");n.stringify=function(r,e){switch(e=e||r.protocol){case"ssh":return"git@"+r.resource+":"+r.full_name+".git";case"git+ssh":return"git+ssh://git@"+r.resource+"/"+r.full_name+".git";case"http":case"https":return e+"://"+r.resource+"/"+r.full_name;default:return r.href}},e.exports=n},{"git-up":2}],2:[function(r,e,t){function n(r){var e=o(r);e.token="";var t=e.user.split(":");return 2===t.length&&("x-oauth-basic"===t[1]?e.token=t[0]:"x-token-auth"===t[0]&&(e.token=t[1])),s(e.protocols)||s(r)?e.protocol="ssh":e.protocols.length?e.protocol=e.protocols[0]:e.protocol="file",e}var o=r("parse-url"),s=r("is-ssh");e.exports=n},{"is-ssh":3,"parse-url":4}],3:[function(r,e,t){function n(r){if(Array.isArray(r))return-1!==r.indexOf("ssh")||-1!==r.indexOf("rsync");if("string"!=typeof r)return!1;var e=o(r);return r=r.substring(r.indexOf("://")+3),n(e)?!0:r.indexOf("@")<r.indexOf(":")}var o=r("protocols");e.exports=n},{protocols:5}],4:[function(r,e,t){function n(r){var e={protocols:o(r),protocol:null,port:null,resource:"",user:"",pathname:"",hash:"",search:"",href:r},t=r.indexOf("://"),n=null,i=null;return e.protocol=e.protocols[0]||(s(r)?"ssh":"file"),-1!==t&&(r=r.substring(t+3)),i=r.split("/"),e.resource=i.shift(),n=e.resource.split("@"),2===n.length&&(e.user=n[0],e.resource=n[1]),n=e.resource.split(":"),2===n.length&&(e.resource=n[0],e.port=parseInt(n[1]),isNaN(e.port)&&(e.port=null,i.unshift(n[1]))),i=i.filter(Boolean),e.pathname="/"+i.join("/"),n=e.pathname.split("#"),2===n.length&&(e.pathname=n[0],e.hash=n[1]),n=e.pathname.split("?"),2===n.length&&(e.pathname=n[0],e.search=n[1]),e}var o=r("protocols"),s=r("is-ssh");e.exports=n},{"is-ssh":3,protocols:5}],5:[function(r,e,t){e.exports=function(r,e){e===!0&&(e=0);var t=r.indexOf("://"),n=r.substring(0,t).split("+").filter(Boolean);return"number"==typeof e?n[e]:n}},{}]},{},[1]);
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.gitUrlParse = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";
+
+// Dependencies
+const gitUp = require("git-up");
+
+/**
+ * gitUrlParse
+ * Parses a Git url.
+ *
+ * @name gitUrlParse
+ * @function
+ * @param {String} url The Git url to parse.
+ * @return {GitUrl} The `GitUrl` object containing:
+ *
+ *  - `protocols` (Array): An array with the url protocols (usually it has one element).
+ *  - `port` (null|Number): The domain port.
+ *  - `resource` (String): The url domain (including subdomains).
+ *  - `user` (String): The authentication user (usually for ssh urls).
+ *  - `pathname` (String): The url pathname.
+ *  - `hash` (String): The url hash.
+ *  - `search` (String): The url querystring value.
+ *  - `href` (String): The input url.
+ *  - `protocol` (String): The git url protocol.
+ *  - `token` (String): The oauth token (could appear in the https urls).
+ *  - `source` (String): The Git provider (e.g. `"github.com"`).
+ *  - `owner` (String): The repository owner.
+ *  - `name` (String): The repository name.
+ *  - `full_name` (String): The owner and name values in the `owner/name` format.
+ *  - `toString` (Function): A function to stringify the parsed url into another url type.
+ *  - `organization` (String): The organization the owner belongs to. This is CloudForge specific.
+ *
+ */
+function gitUrlParse(url) {
+
+    if (typeof url !== "string") {
+        throw new Error("The url must be a string.");
+    }
+
+    let urlInfo = gitUp(url)
+      , sourceParts = urlInfo.resource.split(".")
+      , splits = null
+      ;
+
+    urlInfo.toString = function (type) {
+        return gitUrlParse.stringify(this, type);
+    };
+
+    urlInfo.source = sourceParts.length > 2
+                   ? sourceParts.slice(-2).join(".")
+                   : urlInfo.source = urlInfo.resource
+                   ;
+
+    urlInfo.name = urlInfo.pathname.substring(1).replace(/\.git$/, "");
+    urlInfo.owner = urlInfo.user;
+    urlInfo.organization = urlInfo.owner;
+
+    switch (urlInfo.source) {
+        case "cloudforge.com":
+            urlInfo.owner = urlInfo.user;
+            urlInfo.organization = sourceParts[0];
+            break;
+        default:
+            splits = urlInfo.name.split("/");
+            if (splits.length === 2) {
+                urlInfo.owner = splits[0];
+                urlInfo.name = splits[1];
+            }
+            break;
+    }
+
+    urlInfo.full_name = urlInfo.owner;
+    if (urlInfo.name) {
+        urlInfo.full_name && (urlInfo.full_name += "/");
+        urlInfo.full_name += urlInfo.name;
+    }
+
+    return urlInfo;
+}
+
+/**
+ * stringify
+ * Stringifies a `GitUrl` object.
+ *
+ * @name stringify
+ * @function
+ * @param {GitUrl} obj The parsed Git url object.
+ * @param {String} type The type of the stringified url (default `obj.protocol`).
+ * @return {String} The stringified url.
+ */
+gitUrlParse.stringify = function (obj, type) {
+    type = type || obj.protocol;
+    switch (type) {
+        case "ssh":
+            return `git@${obj.resource}:${obj.full_name}.git`;
+        case "git+ssh":
+            return `git+ssh://git@${obj.resource}/${obj.full_name}.git`;
+        case "http":
+        case "https":
+            return `${type}://${obj.resource}/${obj.full_name}`;
+        default:
+            return obj.href;
+    }
+};
+
+module.exports = gitUrlParse;
+
+},{"git-up":2}],2:[function(require,module,exports){
+"use strict";
+
+// Dependencies
+const parseUrl = require("parse-url")
+    , isSsh = require("is-ssh")
+    ;
+
+/**
+ * gitUp
+ * Parses the input url.
+ *
+ * @name gitUp
+ * @function
+ * @param {String} input The input url.
+ * @return {Object} An object containing the following fields:
+ *
+ *  - `protocols` (Array): An array with the url protocols (usually it has one element).
+ *  - `port` (null|Number): The domain port.
+ *  - `resource` (String): The url domain (including subdomains).
+ *  - `user` (String): The authentication user (usually for ssh urls).
+ *  - `pathname` (String): The url pathname.
+ *  - `hash` (String): The url hash.
+ *  - `search` (String): The url querystring value.
+ *  - `href` (String): The input url.
+ *  - `protocol` (String): The git url protocol.
+ *  - `token` (String): The oauth token (could appear in the https urls).
+ */
+function gitUp(input) {
+    let output = parseUrl(input);
+    output.token = "";
+
+    let splits = output.user.split(":");
+    if (splits.length === 2) {
+        if (splits[1] === "x-oauth-basic") {
+            output.token = splits[0];
+        } else if (splits[0] === "x-token-auth") {
+            output.token = splits[1];
+        }
+    }
+
+    if (isSsh(output.protocols) || isSsh(input)) {
+        output.protocol = "ssh";
+    } else if (output.protocols.length) {
+        output.protocol = output.protocols[0];
+    } else {
+        output.protocol = "file";
+    }
+
+    return output;
+}
+
+module.exports = gitUp;
+
+},{"is-ssh":3,"parse-url":4}],3:[function(require,module,exports){
+// Dependencies
+const protocols = require("protocols");
+
+/**
+ * isSsh
+ * Checks if an input value is a ssh url or not.
+ *
+ * @name isSsh
+ * @function
+ * @param {String|Array} input The input url or an array of protocols.
+ * @return {Boolean} `true` if the input is a ssh url, `false` otherwise.
+ */
+function isSsh(input) {
+
+    if (Array.isArray(input)) {
+        return input.indexOf("ssh") !== -1 || input.indexOf("rsync") !== -1;
+    }
+
+    if (typeof input !== "string") {
+        return false;
+    }
+
+    var prots = protocols(input);
+    input = input.substring(input.indexOf("://") + 3);
+    if (isSsh(prots)) {
+        return true;
+    }
+
+    // TODO This probably could be improved :)
+    return input.indexOf("@") < input.indexOf(":");
+}
+
+module.exports = isSsh;
+
+},{"protocols":5}],4:[function(require,module,exports){
+// Dependencies
+const protocols = require("protocols")
+    , isSsh = require("is-ssh")
+    ;
+
+/**
+ * parseUrl
+ * Parses the input url.
+ *
+ * @name parseUrl
+ * @function
+ * @param {String} url The input url.
+ * @return {Object} An object containing the following fields:
+ *
+ *  - `protocols` (Array): An array with the url protocols (usually it has one element).
+ *  - `protocol` (String): The first protocol, `"ssh"` (if the url is a ssh url) or `"file"`.
+ *  - `port` (null|Number): The domain port.
+ *  - `resource` (String): The url domain (including subdomains).
+ *  - `user` (String): The authentication user (usually for ssh urls).
+ *  - `pathname` (String): The url pathname.
+ *  - `hash` (String): The url hash.
+ *  - `search` (String): The url querystring value.
+ *  - `href` (String): The input url.
+ */
+function parseUrl(url) {
+    var output = {
+            protocols: protocols(url)
+          , protocol: null
+          , port: null
+          , resource: ""
+          , user: ""
+          , pathname: ""
+          , hash: ""
+          , search: ""
+          , href: url
+        }
+      , protocolIndex = url.indexOf("://")
+      , resourceIndex = -1
+      , splits = null
+      , parts = null
+      ;
+
+
+    output.protocol = output.protocols[0] || (isSsh(url) ? "ssh" : "file");
+
+    if (protocolIndex !== -1) {
+        url = url.substring(protocolIndex + 3);
+    }
+
+    parts = url.split("/");
+    output.resource = parts.shift();
+
+    // user@domain
+    splits = output.resource.split("@");
+    if (splits.length === 2) {
+        output.user = splits[0];
+        output.resource = splits[1];
+    }
+
+
+    // domain.com:port
+    splits = output.resource.split(":");
+    if (splits.length === 2) {
+        output.resource = splits[0];
+        output.port = parseInt(splits[1]);
+        if (isNaN(output.port)) {
+            output.port = null;
+            parts.unshift(splits[1]);
+        }
+    }
+
+    // Remove empty elements
+    parts = parts.filter(Boolean);
+
+    // Stringify the pathname
+    output.pathname = "/" + parts.join("/");
+
+    // #some-hash
+    splits = output.pathname.split("#");
+    if (splits.length === 2) {
+        output.pathname = splits[0];
+        output.hash = splits[1];
+    }
+
+    // ?foo=bar
+    splits = output.pathname.split("?");
+    if (splits.length === 2) {
+        output.pathname = splits[0];
+        output.search = splits[1];
+    }
+
+    return output;
+}
+
+module.exports = parseUrl;
+
+},{"is-ssh":3,"protocols":5}],5:[function(require,module,exports){
+/**
+ * protocols
+ * Returns the protocols of an input url.
+ *
+ * @name protocols
+ * @function
+ * @param {String} input The input url.
+ * @param {Boolean|Number} first If `true`, the first protocol will be returned. If number, it will represent the zero-based index of the protocols array.
+ * @return {Array|String} The array of protocols or the specified protocol.
+ */
+module.exports = function protocols(input, first) {
+
+    if (first === true) {
+        first = 0;
+    }
+
+    var index = input.indexOf("://")
+      , splits = input.substring(0, index).split("+").filter(Boolean)
+      ;
+
+    if (typeof first === "number") {
+        return splits[first];
+    }
+
+    return splits;
+};
+
+},{}]},{},[1])(1)
+});
