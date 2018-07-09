c4dfff95a9c0065601d27a7f8b29cb10f2381a23
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-07-09 16:31:17 -0400

    Revert "Fix #65. Check if the parsed resource is not null."
    This reverts commit f10ded0d4404873604afb61326f5194b7c98f9bd.

--- test/index.js
+++ test/index.js
@@ -281,13 +281,8 @@ tester.describe("parse urls", test => {
         test.expect(res.toString()).toBe("http://user@github.com/owner/name.git");
     });
 
-    test.it("custom url", () => {
+    test.should("custom url", () => {
         var res = gitUrlParse("https://git.test.com.cn/a/b");
         test.expect(res.source).toBe('test.com.cn');
-    })
-
-    test.it("local urls", () => {
-        var res = gitUrlParse("http://machine:8080/tfs/Collection/Project.Name/_git/repository");
-        test.expect(res.source).toBe('machine');
     })
 });
